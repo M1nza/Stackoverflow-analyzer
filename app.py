5dcfe0af25506f67716ca1edc690062b8c3b27d8
Minza Sadiq <140895481+M1nza@users.noreply.github.com>
2025-07-01 17:42:00 +0400

    app.py

--- app.py
+++ app.py
@@ -26,12 +26,27 @@ def fetch_questions(tag, pagesize=50):
         return []
 
 def extract_keywords(titles):
+    stopwords = set([
+        "this", "that", "with", "from", "they", "would", "there", "their",
+        "what", "which", "your", "have", "will", "about", "could", "should",
+        "because", "while", "where", "when", "then", "them", "some", "just",
+        "into", "also", "only", "being", "been", "more", "like", "than",
+        "each", "most", "many", "such", "over", "other", "after", "even",
+        "using", "make", "much", "does", "well", "can't", "don't", "isn't",
+        "i've", "i'm", "you're", "it's", "did", "was", "were", "has", "had",
+        "the", "and", "for", "you", "are", "but", "not", "all", "any", "can",
+        "its", "get", "out", "too", "how", "why", "who", "what", "when", "which",
+        "an", "a", "of", "in", "to", "on", "if", "as", "at", "by", "or", "be", "do", "is", "no"
+    ])
+
     all_words = []
     for title in titles:
-        words = re.findall(r'\b[a-zA-Z]{4,}\b', title.lower())  # words with 4+ letters
-        all_words.extend(words)
+        words = re.findall(r'\b[a-zA-Z]{4,}\b', title.lower())
+        filtered = [w for w in words if w not in stopwords]
+        all_words.extend(filtered)
     common = Counter(all_words).most_common(15)
     return common
+
 
 if st.button("Analyze"):
     st.info(f"Fetching recent questions tagged with '{tag}'...")
